9e5b4f0a2f2a231594fed33a246d1ce82700f5b4
Quang Tran <nhatquang0672@gmail.com>
2023-01-02 18:08:31 +0700

    Update

--- kvs-p1/src/bin/kvs.rs
+++ kvs-p1/src/bin/kvs.rs
@@ -1,3 +1,17 @@
+use clap::{Parser, Subcommand, Args};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands 
+}
+
+#[derive(Subcommand)]
+enum Commands {}
+
+
+
 fn main() {
+    kvs::KvStore;
     println!("Hello, world!")
 }
